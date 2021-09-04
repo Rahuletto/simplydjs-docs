24cde6abbc49b9e4e91768722a89f015acb482fb
Rahul <rahulmarban@gmail.com>
2021-09-04 17:24:50 +0530

    aaaaa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
     throwIfNamespace: false,
     metadatas: [
       {
-        name: 'google-site-verification',  content="pb2-QCQjmdU-4KOGjcrazLLDeY9XDxG25NONdsbbt4I"
+        name: 'google-site-verification',  content: "pb2-QCQjmdU-4KOGjcrazLLDeY9XDxG25NONdsbbt4I"
       },
       {
         property: 'og:title', content: 'Simply DJS'
