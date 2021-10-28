ac0e7073d0c2176a032134315748171132089599
Rahul <rahulmarban@gmail.com>
2021-10-28 18:35:05 +0530

    Idk

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ module.exports = {
     throwIfNamespace: false,
 scripts: [
     {
-      data-ad-client="ca-pub-3749536571184673",
-     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      data-ad-client: "ca-pub-3749536571184673",
+     src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
       async: true,
     },
   ],
