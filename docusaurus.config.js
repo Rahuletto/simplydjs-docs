e78ded1e1f81ce66655f49b77800c6b4e6e9f47e
Rahul Marban <rahulmarban@gmail.com>
2022-04-06 07:39:23 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,68 @@
-const lightCodeTheme = require("./dark.js");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = { 
+ ​  ​plain​: ​{ 
+ ​    ​color: '#abb2bf'​, 
+ ​    ​backgroundColor​: ​'#282c34'​, 
+ ​  ​}​, 
+ ​  ​styles​: ​[ 
+     ...darky.styles,
+ ​    ​{ 
+ ​      ​types​: ​[​'title'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#abb2bf'​, 
+ ​        ​fontWeight​: ​'bold'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'property'​,​ ​'parameter'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e06c75'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'script'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#D4D4D4'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'arrow'​,​ ​'atrule'​,​ ​'tag'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#56b6c2'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'number', 'boolean'​,​ ​'color'​,​ ​'unit'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#d19a66'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'keyword'​,​ ​'rule'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#c678dd'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'regex', 'font-matter'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#98c379'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'maybe-class-name'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e06c75'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'constant'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e5c07b'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​  ​]​, 
+ ​}​;
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
