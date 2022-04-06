2ffa34578c22e447070631a02f489d4ac4fcd937
Rahul Marban <rahulmarban@gmail.com>
2022-04-06 07:32:08 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
+const lightCodeTheme = require("./dark.js");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
