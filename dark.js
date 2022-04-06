0ba4701028374c49c5120f04485f8e6022875850
Rahul Marban <rahulmarban@gmail.com>
2022-04-06 07:33:31 +0530

    Create dark.js

---
+++ dark.js
@@ -0,0 +1,66 @@
+ let darky = require('prism-react-renderer/themes/vsDark/index.cjs.js')
+
+module.exports = { 
+ ​  ​plain​: ​{ 
+ ​    ​color: '#abb2bf'​, 
+ ​    ​backgroundColor​: ​'#282c34'​, 
+ ​  ​}​, 
+ ​  ​styles​: ​[ 
+     ...darky.styles,
+ ​    ​{ 
+ ​      ​types​: ​[​'title'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#abb2bf'​, 
+ ​        ​fontWeight​: ​'bold'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'property'​,​ ​'parameter'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e06c75'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'script'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#D4D4D4'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'arrow'​,​ ​'atrule'​,​ ​'tag'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#56b6c2'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'number', 'boolean'​,​ ​'color'​,​ ​'unit'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#d19a66'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'keyword'​,​ ​'rule'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#c678dd'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'regex', 'font-matter'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#98c379'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'maybe-class-name'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e06c75'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​    ​{ 
+ ​      ​types​: ​[​'constant'​]​, 
+ ​      ​style​: ​{ 
+ ​        ​color​: ​'#e5c07b'​, 
+ ​      ​}​, 
+ ​    ​}​, 
+ ​  ​]​, 
+ ​}​;
