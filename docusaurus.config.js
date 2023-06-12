95d15787e9f256922e0ae98d64edce9cf872f874
Rahul Marban <rahulmarban@gmail.com>
2023-06-12 12:17:15 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ module.exports = {
     algolia: {
       apiKey: "91f2393262c617a2955e3550c006bc4d",
       appId: "0V6L7HOWNI",
-      indexName: "simplyd",
+      indexName: "netlify_1bec4261-2817-4fc6-9fbc-ffbce0d1ab57_v4_all",
       ignoreCanonicalTo: true,
       inputSelector: "h1",
       searchParameters: {
